faca9390aea5482cfb8a238783e550499ebc1bd9
JitenRaj <ji10raj@gmail.com>
2026-03-09 03:13:12 +0530

    Update: integrate Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@
 import { Header } from './components/Header';
 import { InputArea } from './components/InputArea';
 import { ChatArea } from './components/ChatArea';
+import { Sidebar } from './components/Sidebar';
 
 export default function App() {
   return (
     <div className="flex h-screen w-full bg-wizzy-bg overflow-hidden text-wizzy-text-main font-sans">
+
+      <Sidebar />
 
       <div className="flex-1 flex flex-col h-full relative min-w-0">
         <Header />
